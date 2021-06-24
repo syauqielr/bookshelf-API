e728808253b9ca9fbc0081e6f047892f8b8ce300
Syauqielr <syauqielrahman@gmail.com>
2021-06-24 17:31:55 +0700

    penambahan file pada src

---
+++ src/handler.js
@@ -0,0 +1,50 @@
+const {nanoid} = require('nanoid');
+
+const addBooksHandler = (request, h) => {
+  const {title, tags, body} = request.payload;
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  const newNBooks = {
+    title,
+    tags,
+    body,
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+  newNBooks.finished;
+
+  books.push(newNBooks);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
