e73a6e20387b7f7b7837c54a4e7fd140e2dd01e5
Syauqielr <syauqielrahman@gmail.com>
2021-06-24 18:25:21 +0700

    menambahkan pengambilan book dengan id

--- src/handler.js
+++ src/handler.js
@@ -79,4 +79,26 @@ const getAllBooksHandler = () => ({
   },
 });
 
-module.exports = {addBooksHandler, getAllBooksHandler};
+const getBooksByIdHandler = (request, h) => {
+  const {id} = request.params;
+
+  const book = books.filter((n) => n.id === id)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {addBooksHandler, getAllBooksHandler, getBooksByIdHandler};
